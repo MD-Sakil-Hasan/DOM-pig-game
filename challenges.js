81477d04be86102ff79f33b1753a15a81dc150db
MD-Sakil-Hasan <mdsakilhasan833@gmail.comm>
2020-07-27 15:08:26 +0600

    3rd Challenge Completed

--- challenges.js
+++ challenges.js
@@ -4,6 +4,9 @@ And 3 CHALLENGES:
 1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
 2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
 3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+
+
+**In order to save the previous code (app.js) I created a new file (challenges.js) and copied the previous code to complete the challenges.  
 */
 
 
@@ -16,37 +19,54 @@ init()
 
 var lastDice;
 
-document.querySelector(".dice").style.display = "none";
 
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
+// document.querySelector(".dice").style.display = "none";
+
+// document.getElementById("score-0").textContent = "0";
+// document.getElementById("score-1").textContent = "0";
+// document.getElementById("current-0").textContent = "0";
+// document.getElementById("current-1").textContent = "0";
+
 
 document.querySelector(".btn-roll").addEventListener("click", function () {
     if(gamePlaying){
         //Random Number
-        var dice = Math.floor(Math.random() * 6) + 1;
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
 
         //Display the result
-        var diceDOM = document.querySelector(".dice");
-        diceDOM.style.display = "block";
-        diceDOM.src = "dice-" + dice + ".png";
 
-        //1st challenge done here and lastDice variable added to store the last rolled score.
-        if(dice === 6 && lastDice === 6){
-            scores[activePlayer] = 0;
-            document.querySelector('#score-' + activePlayer).textContent = '0';
-            nextPlayer()
-        }
-        //Update the round score if the rolled  number was not 1
-        else if (dice !== 1) {
-            roundScore += dice;
-            document.querySelector("#current-" + activePlayer).textContent = roundScore;
-        } else {
-            nextPlayer();
-        }
-        lastDice = dice;
+        document.getElementById('dice-1').style.display = 'block'
+        document.getElementById('dice-2').style.display = 'block'
+
+        document.getElementById('dice-1').src = "dice-" + dice1 + ".png";
+        document.getElementById('dice-2').src = "dice-" + dice2 + ".png";
+
+
+
+    
+    if (dice1 !== 1 && dice2 !== 1) {
+        roundScore += dice1 + dice2;
+        document.querySelector("#current-" + activePlayer).textContent = roundScore;
+    } else {
+        nextPlayer();
+    }
+
+
+    //1st challenge done here and lastDice variable added to store the last rolled score.
+    //     if(dice === 6 && lastDice === 6){
+    //         scores[activePlayer] = 0;
+    //         document.querySelector('#score-' + activePlayer).textContent = '0';
+    //         nextPlayer()
+    //     }
+    //     //Update the round score if the rolled  number was not 1
+    //     else if (dice !== 1) {
+    //         roundScore += dice;
+    //         document.querySelector("#current-" + activePlayer).textContent = roundScore;
+    //     } else {
+    //         nextPlayer();
+    //     }
+    //     lastDice = dice;
     }
     
     
@@ -60,9 +80,19 @@ document.querySelector(".btn-hold").addEventListener('click', function () {
 
         document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
 
-        if(scores[activePlayer] >= 100){
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+
+        if(input){
+            winningScore = input;
+        } else {
+            winningScore = 100;
+        }
+
+        if(scores[activePlayer] >= winningScore){
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!!'
-            document.querySelector('.dice').style.display = 'none';
+            document.getElementById('dice-1').style.display = 'none'
+            document.getElementById('dice-2').style.display = 'none'
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
             gamePlaying = false;
@@ -76,7 +106,7 @@ document.querySelector(".btn-hold").addEventListener('click', function () {
 
 
 function nextPlayer() {
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
 
     document.getElementById("current-0").textContent = "0";
@@ -85,7 +115,8 @@ function nextPlayer() {
     document.querySelector(".player-0-panel").classList.toggle("active");
     document.querySelector(".player-1-panel").classList.toggle("active");
 
-    document.querySelector(".dice").style.display = "none";
+    document.getElementById('dice-1').style.display = 'none'
+    document.getElementById('dice-2').style.display = 'none'
 }
 
 
@@ -99,7 +130,8 @@ function init(){
 
     gamePlaying = true;
 
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none'
+    document.getElementById('dice-2').style.display = 'none'
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
@@ -116,3 +148,4 @@ function init(){
 
 
 
+// 3rd challenge changes more code than previous challenges and remove "continuous two 6 reset all score" rule. It also added an extra dice and for that I had to changes their value and display setting several time in code and that made a mess and I will try to solve it with function in order to follow DRY principle.
